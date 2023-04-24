cbb10a6b231b15f2a5c687c32f436c0295578819
AMKovcheg <n.dundukov@yandex.ru>
2023-04-24 05:44:28 +0300

    ddl fixed

--- cmd/db/scripts/ddl.sql
+++ cmd/db/scripts/ddl.sql
@@ -1,66 +1,33 @@
 CREATE SCHEMA IF NOT EXISTS bshop;
 
-<<<<<<< HEAD
-CREATE TABLE IF NOT EXISTS bshop.user (
-  user_id SERIAL UNIQUE,
-  firstname VARCHAR(100) NOT NULL,
-  surname VARCHAR(100) NOT NULL,
-  phone_number VARCHAR(20) NOT NULL,
-  email VARCHAR(200) UNIQUE NOT NULL,
-=======
 CREATE TABLE bshop.user (
   user_id INTEGER PRIMARY KEY,
   firstname VARCHAR(100) NOT NULL,
   surname VARCHAR(100) NOT NULL,
   phone_number VARCHAR(20) NOT NULL,
   email VARCHAR(200) NOT NULL,
->>>>>>> 793bb79 (ddl added)
   birthdate DATE NOT NULL,
   hashed_password VARCHAR(200) NOT NULL
 );
 
-<<<<<<< HEAD
-CREATE TABLE IF NOT EXISTS bshop.product (
-  product_id SERIAL UNIQUE,
-=======
 CREATE TABLE bshop.product (
   product_id INTEGER PRIMARY KEY,
->>>>>>> 793bb79 (ddl added)
   category VARCHAR(100) NOT NULL,
   name VARCHAR(100) NOT NULL,
   brand VARCHAR(50),
   price INTEGER NOT NULL,
   available BOOLEAN NOT NULL,
-<<<<<<< HEAD
-  rating NUMERIC NOT NULL,
-  CHECK (rating <= 10)
-);
-
-CREATE TABLE IF NOT EXISTS bshop.review (
-  review_id SERIAL UNIQUE,
-=======
   rating NUMERIC NOT NULL
 );
 
 CREATE TABLE bshop.review (
   review_id INTEGER PRIMARY KEY,
->>>>>>> 793bb79 (ddl added)
   user_id INTEGER NOT NULL,
   comment TEXT,
   product_id INTEGER NOT NULL,
   date DATE NOT NULL,
   mark INTEGER NOT NULL,
   FOREIGN KEY (user_id) REFERENCES bshop.user(user_id) ON DELETE CASCADE,
-<<<<<<< HEAD
-  FOREIGN KEY (product_id) REFERENCES bshop.product(product_id) ON DELETE CASCADE,
-  CHECK (mark >= 1 and mark <= 10)
-);
-
-CREATE TABLE IF NOT EXISTS bshop.purchase (
-  purchase_id SERIAL UNIQUE,
-  client_id INTEGER NOT NULL,
-  purchase_price INTEGER NOT NULL,
-=======
   FOREIGN KEY (product_id) REFERENCES bshop.product(product_id) ON DELETE CASCADE
 );
 
@@ -73,13 +40,8 @@ CREATE TABLE bshop.purchase (
   FOREIGN KEY (client_id) REFERENCES bshop.user(user_id) ON DELETE CASCADE
 );
 
-<<<<<<< HEAD
-CREATE TABLE IF NOT EXISTS bshop.purchase_product (
-  pp_id SERIAL UNIQUE,
-=======
 CREATE TABLE bshop.purchase_product (
   pp_id INTEGER PRIMARY KEY,
->>>>>>> 793bb79 (ddl added)
   purchase_id INTEGER NOT NULL,
   product_id INTEGER NOT NULL,
   amount TEXT NOT NULL,
@@ -87,11 +49,7 @@ CREATE TABLE bshop.purchase_product (
   FOREIGN KEY (product_id) REFERENCES bshop.product(product_id) ON DELETE CASCADE
 );
 
-<<<<<<< HEAD
-CREATE TABLE IF NOT EXISTS bshop.user_history (
-=======
 CREATE TABLE bshop.user_history (
->>>>>>> 793bb79 (ddl added)
   update_id INTEGER PRIMARY KEY,
   user_id INTEGER NOT NULL,
   phone_number VARCHAR(20) NOT NULL,
