dc9a49ddee707050bb0bf5bd9b8225e63690e13e
AMKovcheg <n.dundukov@yandex.ru>
2023-04-24 05:44:28 +0300

    ddl fixed

--- cmd/db/scripts/ddl.sql
+++ cmd/db/scripts/ddl.sql
@@ -123,6 +123,120 @@ CREATE TABLE IF NOT EXISTS bshop.product_history (
   old_rating NUMERIC,
   FOREIGN KEY (product_id) REFERENCES bshop.product(product_id) ON DELETE CASCADE
 );
+CREATE SCHEMA IF NOT EXISTS bshop;
+
+CREATE TABLE bshop.user (
+  user_id INTEGER PRIMARY KEY,
+  firstname VARCHAR(100) NOT NULL,
+  surname VARCHAR(100) NOT NULL,
+  phone_number VARCHAR(20) NOT NULL,
+  email VARCHAR(200) NOT NULL,
+  birthdate DATE NOT NULL,
+  hashed_password VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE bshop.product (
+  product_id INTEGER PRIMARY KEY,
+  category VARCHAR(100) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  brand VARCHAR(50),
+  price INTEGER NOT NULL,
+  available BOOLEAN NOT NULL,
+  rating NUMERIC NOT NULL,
+  CHECK (rating <= 10)
+);
+
+CREATE TABLE bshop.review (
+  review_id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  comment TEXT,
+  product_id INTEGER NOT NULL,
+  date DATE NOT NULL,
+  mark INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES bshop.user(user_id) ON DELETE CASCADE,
+  FOREIGN KEY (product_id) REFERENCES bshop.product(product_id) ON DELETE CASCADE,
+  CHECK (mark >= 1 and mark <= 10)
+);
+
+CREATE TABLE bshop.purchase (
+  purchase_id INTEGER PRIMARY KEY,
+  client_id INTEGER NOT NULL,
+  purchase_price INTEGER NOT NULL,
+  date DATE NOT NULL,
+  FOREIGN KEY (client_id) REFERENCES bshop.user(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE bshop.purchase_product (
+  pp_id INTEGER PRIMARY KEY,
+  purchase_id INTEGER NOT NULL,
+  product_id INTEGER NOT NULL,
+  amount TEXT NOT NULL,
+  FOREIGN KEY (purchase_id) REFERENCES bshop.purchase(purchase_id) ON DELETE CASCADE,
+  FOREIGN KEY (product_id) REFERENCES bshop.product(product_id) ON DELETE CASCADE
+);
+
+CREATE TABLE bshop.user_history (
+  update_id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  phone_number VARCHAR(20) NOT NULL,
+  old_phone_number VARCHAR(20) NOT NULL,
+  email VARCHAR(200) NOT NULL,
+  old_email VARCHAR(200) NOT NULL,
+  hashed_password VARCHAR(200) NOT NULL,
+  old_hashed_password VARCHAR(200) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES bshop.user(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE bshop.product (
+  product_id INTEGER PRIMARY KEY,
+  category VARCHAR(100) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  brand VARCHAR(50),
+  price INTEGER NOT NULL,
+  available BOOLEAN NOT NULL,
+  rating NUMERIC NOT NULL
+);
+
+CREATE TABLE bshop.review (
+  review_id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  comment TEXT,
+  product_id INTEGER NOT NULL,
+  date DATE NOT NULL,
+  mark INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES bshop.user(user_id) ON DELETE CASCADE,
+  FOREIGN KEY (product_id) REFERENCES bshop.product(product_id) ON DELETE CASCADE,
+  CHECK (mark >= 1 and mark <= 10)
+);
+
+CREATE TABLE bshop.purchase (
+  purchase_id INTEGER PRIMARY KEY,
+  client_id INTEGER NOT NULL,
+  purchas_eprice INTEGER NOT NULL,
+  date DATE NOT NULL,
+  FOREIGN KEY (client_id) REFERENCES bshop.user(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE bshop.purchase_product (
+  pp_id INTEGER PRIMARY KEY,
+  purchase_id INTEGER NOT NULL,
+  product_id INTEGER NOT NULL,
+  amount TEXT NOT NULL,
+  FOREIGN KEY (purchase_id) REFERENCES bshop.purchase(purchase_id) ON DELETE CASCADE,
+  FOREIGN KEY (product_id) REFERENCES bshop.product(product_id) ON DELETE CASCADE
+);
+
+CREATE TABLE bshop.user_history (
+  update_id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  phone_number VARCHAR(20) NOT NULL,
+  old_phone_number VARCHAR(20) NOT NULL,
+  email VARCHAR(200) NOT NULL,
+  old_email VARCHAR(200) NOT NULL,
+  hashed_password VARCHAR(200) NOT NULL,
+  old_hashed_password VARCHAR(200) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES bshop.user(user_id) ON DELETE CASCADE
+);
 
 SELECT name, brand, category, rating,
 							price, available FROM bshop.product;
