0508295c1201ee7468e0afb55b610e0761eb489a
kuzin roman <rkuzin.2003@gmail.com>
2023-04-30 22:12:31 +0300

    fix test + fix communication between front and back

--- ui/static/js/auth_submit.js
+++ ui/static/js/auth_submit.js
@@ -25,20 +25,36 @@ function setCookie(name, value, options = {}) {
 
 function auth_submit() {
     var inputForm = document.getElementById("auth_form");
-    inputForm.addEventListener("submit", (e)=> {
+    inputForm.addEventListener("submit", async (e)=> {
         e.preventDefault()
         
         const formData = new FormData(inputForm)
+        console.log(formData.get("email"))
 
         fetch("/auth", {
             method: "POST",
-            body: formData,
+            body: JSON.stringify({email: formData.get("email"), password: formData.get("password")}),
         }).then(
             response => response.text()
         ).then(
-            (data) => {console.log(data); document.getElementById("serverMessageBox").innerHTML = data}
+            (data) => {console.log(data);document.getElementById("serverMessageBox").innerHTML=data}
         ).catch(
             error => console.error(error)
         )
+    
+
+        // // (async () => {
+        //     const rawResponse = await fetch('/auth', {
+        //       method: 'POST',
+        //       headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/json'
+        //       },
+        //       body: JSON.stringify({a: 1, b: 'Textual content'})
+        //     });
+        //     const content = await rawResponse.json();
+          
+        //     console.log(content);
+         // })();
     });
 }
