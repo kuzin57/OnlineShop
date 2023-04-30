ad386809ff7d27a2c16635dce86f3173caba6a57
AMKovcheg <n.dundukov@yandex.ru>
2023-04-30 14:32:06 +0300

    ddl fixed

--- cmd/db/scripts/ddl.sql
+++ cmd/db/scripts/ddl.sql
@@ -88,7 +88,7 @@ CREATE TABLE IF NOT EXISTS bshop.review (
 CREATE TABLE bshop.purchase (
   purchase_id INTEGER PRIMARY KEY,
   client_id INTEGER NOT NULL,
-  purchas_eprice INTEGER NOT NULL,
+  purchase_price INTEGER NOT NULL,
   date DATE NOT NULL,
   FOREIGN KEY (client_id) REFERENCES bshop.user(user_id) ON DELETE CASCADE
 );
