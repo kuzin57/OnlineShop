6c98b6a708cef29dfcd1dbb6d4815c83bd50f6e4
kuzin roman <rkuzin.2003@gmail.com>
2023-04-30 22:12:31 +0300

    fix test + fix communication between front and back

--- cmd/handlers/auth.go
+++ cmd/handlers/auth.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 <<<<<<< HEAD
+<<<<<<< HEAD
 	"encoding/json"
 	"net/http"
 	"strings"
@@ -10,12 +11,20 @@ import (
 	"github.com/kuzin57/OnlineShop/cmd/db"
 	"github.com/kuzin57/OnlineShop/cmd/services"
 =======
+=======
+	"encoding/json"
+>>>>>>> 355ec40 (fix test + fix communication between front and back)
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+<<<<<<< HEAD
 	"time"
 >>>>>>> 8a6f0f3 (auth_submit fix)
+=======
+
+	"github.com/kuzin57/OnlineShop/cmd/auth"
+>>>>>>> 355ec40 (fix test + fix communication between front and back)
 )
 
 type authPageHandler struct {
@@ -49,6 +58,7 @@ func AddAuthPageHandler(
 	return handler
 }
 
+<<<<<<< HEAD
 func (h *authPageHandler) Handle(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
@@ -81,5 +91,47 @@ func (h *authPageHandler) Handle(w http.ResponseWriter, r *http.Request) {
 
 		response.Token = token
 		sendResponse(w, response)
+=======
+func (s htmlSources) authPageHandler(w http.ResponseWriter, r *http.Request) {
+	logError := func(err error, w http.ResponseWriter) {
+		fmt.Println("error")
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+
+	switch r.Method {
+	case http.MethodGet:
+		ts, err := template.ParseFiles(s...)
+
+		if err != nil || ts == nil {
+			logError(err, w)
+		}
+
+		if err = ts.Execute(w, nil); err != nil {
+			logError(err, w)
+		}
+
+	case http.MethodPost:
+		user := auth.User{}
+		body := make([]byte, 1000)
+		bytes, _ := r.Body.Read(body)
+
+		body = body[:bytes]
+		fmt.Println("body:", string(body))
+
+		w.Header().Set("Content-Type", "text")
+		response := Response{Status: http.StatusAccepted, Description: "Success!"}
+		if err := auth.Login(&user); err != nil {
+			response.Status = http.StatusForbidden
+			response.Description = err.Error()
+		}
+
+		js, err := json.Marshal(&response)
+		if err != nil {
+			logError(err, w)
+		}
+
+		fmt.Println("response", string(js))
+		w.Write([]byte(js))
 	}
 }
