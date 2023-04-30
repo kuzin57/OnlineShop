25e31f23e2f495b8e80bf7927b97e5b81bde9efd
AMKovcheg <n.dundukov@yandex.ru>
2023-04-30 14:02:52 +0300

    ddl fixed

--- cmd/db/scripts/ddl.sql
+++ cmd/db/scripts/ddl.sql
@@ -69,7 +69,8 @@ CREATE TABLE IF NOT EXISTS bshop.product (
   brand VARCHAR(50),
   price INTEGER NOT NULL,
   available BOOLEAN NOT NULL,
-  rating NUMERIC NOT NULL
+  rating NUMERIC NOT NULL,
+  CHECK (rating <= 5)
 );
 
 CREATE TABLE IF NOT EXISTS bshop.review (
