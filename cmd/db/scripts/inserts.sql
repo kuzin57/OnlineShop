95ba45f65939c54d90dd8241d9b2caa97637915a
AMKovcheg <n.dundukov@yandex.ru>
2023-04-30 13:58:57 +0300

    inserts in process

---
+++ cmd/db/scripts/inserts.sql
@@ -0,0 +1,12 @@
+INSERT INTO bshop.user (user_id, firstname, surname, phone_number, email, birthdate, hashed_password) 
+VALUES 
+(1, 'John', 'Doe', '9234555890', 'johndoe@gmail.com', '1990-01-01', 'sMkLxJyKwRnFpE'),
+(2, 'Jane', 'Doe', '7983654821', 'janedoe@gmail.com', '1992-05-15', 'qZzGtHcXvIbNfD'),
+(3, 'Bob', 'Smith', '5654274869', 'bobsmith@gmail.com', '1985-11-30', 'rTjUoPwQkLmYnB'),
+(4, 'Alice', 'Johnson', '5553575123', 'alicejohnson@gmail.com', '1998-03-22', 'fGhJkLpOzXcVbN'),
+(5, 'David', 'Lee', '7975651801', 'davidlee@gmail.com', '1982-09-10', 'aSdFgHjKlMnBvC'),
+(6, 'Sarah', 'Kim', '1415278080', 'sarahkim@gmail.com', '1995-06-18', 'eRtYhUjIiOpLkN'),
+(7, 'Michael', 'Brown', '7094541363', 'michaelbrown@gmail.com', '1978-12-25', 'dFgHjKlMnBvCxZ'),
+(8, 'Emily', 'Davis', '4518320073', 'emilydavis@gmail.com', '1991-08-07', 'qWeRtYuIoPzXcVb'),
+(9, 'James', 'Wilson', '1528384959', 'jameswilson@gmail.com', '1980-04-12', 'bNmJkLoPqRsTfGh'),
+(10, 'Jessica', 'Taylor', '9581637854', 'jessicataylor@gmail.com', '1993-02-28', 'yHjKlMnBvCxZqW');
