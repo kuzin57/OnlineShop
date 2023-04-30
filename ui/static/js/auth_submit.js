ba7e9b9fc20719f14f2ec1d21f1874da2f36fac1
kuzin roman <rkuzin.2003@gmail.com>
2023-04-30 23:10:59 +0300

    removed useless sql scripts

--- ui/static/js/auth_submit.js
+++ ui/static/js/auth_submit.js
@@ -1,7 +1,4 @@
 function auth_submit() {
-    // let form = document.forms["auth_form"];
-    const url = "/127.0.0.1:7000/auth"
-
     var inputForm = document.getElementById("auth_form");
     inputForm.addEventListener("submit", async (e)=> {
         e.preventDefault()
@@ -13,26 +10,19 @@ function auth_submit() {
             method: "POST",
             body: JSON.stringify({email: formData.get("email"), password: formData.get("password")}),
         }).then(
-            response => response.text()
+            response => response.json()
         ).then(
-            (data) => {console.log(data);document.getElementById("serverMessageBox").innerHTML=data}
+            (data) => {
+                console.log(data)
+                if (data.status == 403) {
+                    document.getElementById("serverMessageBox").innerHTML="Login failed!"
+                } else {
+                    document.getElementById("serverMessageBox").innerHTML="Login succeeded!"
+                }
+            }
         ).catch(
             error => console.error(error)
         )
-    
-
-        // // (async () => {
-        //     const rawResponse = await fetch('/auth', {
-        //       method: 'POST',
-        //       headers: {
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json'
-        //       },
-        //       body: JSON.stringify({a: 1, b: 'Textual content'})
-        //     });
-        //     const content = await rawResponse.json();
-          
-        //     console.log(content);
-         // })();
+        
     });
 }
