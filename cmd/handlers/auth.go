021580b081811f6942865f0e7588b798579130f7
kuzin roman <rkuzin.2003@gmail.com>
2023-04-30 22:12:31 +0300

    fix test + fix communication between front and back

--- cmd/handlers/auth.go
+++ cmd/handlers/auth.go
@@ -1,11 +1,13 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"time"
+
+	"github.com/kuzin57/OnlineShop/cmd/auth"
 )
 
 type Response struct {
@@ -21,59 +23,45 @@ func AddAuthPageHandler(router *http.ServeMux, conf PagesConfig) {
 }
 
 func (s htmlSources) authPageHandler(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles(s...)
-
 	logError := func(err error, w http.ResponseWriter) {
 		fmt.Println("error")
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
 
-	if err != nil || ts == nil {
-		logError(err, w)
-	}
+	switch r.Method {
+	case http.MethodGet:
+		ts, err := template.ParseFiles(s...)
 
-	if err = ts.Execute(w, nil); err != nil {
-		logError(err, w)
-	}
+		if err != nil || ts == nil {
+			logError(err, w)
+		}
 
-	// user := auth.User{}
-	if r.Method == http.MethodPost {
-		// body := make([]byte, 1000)
-		// bytes, _ := r.Body.Read(body)
+		if err = ts.Execute(w, nil); err != nil {
+			logError(err, w)
+		}
 
-		// body = body[:bytes]
-		// if err = json.Unmarshal(body, &user); err != nil {
-		// 	logError(err, w)
-		// }
+	case http.MethodPost:
+		user := auth.User{}
+		body := make([]byte, 1000)
+		bytes, _ := r.Body.Read(body)
 
-		// fmt.Println("body:", string(body))
+		body = body[:bytes]
+		fmt.Println("body:", string(body))
 
-		// w.Header().Set("Content-Type", "text")
-		// response := Response{Status: http.StatusAccepted, Description: "Success!"}
-		// // if err = auth.Login(&user); err != nil {
-		// // 	response.Status = http.StatusForbidden
-		// // 	response.Description = err.Error()
-		// // }
+		w.Header().Set("Content-Type", "text")
+		response := Response{Status: http.StatusAccepted, Description: "Success!"}
+		if err := auth.Login(&user); err != nil {
+			response.Status = http.StatusForbidden
+			response.Description = err.Error()
+		}
 
-		// js, err := json.Marshal(&response)
-		// if err != nil {
-		// 	logError(err, w)
-		// }
+		js, err := json.Marshal(&response)
+		if err != nil {
+			logError(err, w)
+		}
 
-		// // // json.NewEncoder(w).Encode(&response)
-		// // // w.Write([]byte("ahahahah"))
-		// // // n, err := w.Write([]byte(js))
-		// // // fmt.Println("bytes written", n, err)
-		// // // fmt.Println(err)
-		// // fmt.Fprintf()
-		// // fmt.Fprintf(w, "hahahaha")
-
-		// fmt.Println("response", string(js))
-		// w.Write([]byte(js))
-		r.ParseMultipartForm(0)
-
-		message := r.FormValue("message")
-		fmt.Fprintf(w, "Server %s \n", message+"|"+time.Now().Format(time.RFC1123))
+		fmt.Println("response", string(js))
+		w.Write([]byte(js))
 	}
 }
--- ui/static/js/auth_submit.js
+++ ui/static/js/auth_submit.js
@@ -3,20 +3,36 @@ function auth_submit() {
     const url = "/127.0.0.1:7000/auth"
 
     var inputForm = document.getElementById("auth_form");
-    inputForm.addEventListener("submit", (e)=> {
+    inputForm.addEventListener("submit", async (e)=> {
         e.preventDefault()
         
         const formData = new FormData(inputForm)
+        console.log(formData.get("email"))
 
         fetch("/auth", {
             method: "POST",
-            body: formData,
+            body: JSON.stringify({email: formData.get("email"), password: formData.get("password")}),
         }).then(
             response => response.text()
         ).then(
-            (data) => {console.log(data); document.getElementById("serverMessageBox").innerHTML = data}
+            (data) => {console.log(data);document.getElementById("serverMessageBox").innerHTML=data}
         ).catch(
             error => console.error(error)
         )
+    
+
+        // // (async () => {
+        //     const rawResponse = await fetch('/auth', {
+        //       method: 'POST',
+        //       headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/json'
+        //       },
+        //       body: JSON.stringify({a: 1, b: 'Textual content'})
+        //     });
+        //     const content = await rawResponse.json();
+          
+        //     console.log(content);
+         // })();
     });
 }
