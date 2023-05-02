d05a30aca5f39d6b8b45d5b4b1c7fce4b25b014a
AMKovcheg <n.dundukov@yandex.ru>
2023-05-02 05:00:58 +0300

    triggers in expected version

--- cmd/db/scripts/triggers.sql
+++ cmd/db/scripts/triggers.sql
@@ -20,6 +20,7 @@ CREATE OR REPLACE TRIGGER user_update_on_delete
 AFTER DELETE ON bshop.user
 FOR EACH ROW EXECUTE FUNCTION bshop.write_user_history_on_delete();
 
+------------------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION bshop.write_user_history_on_insert_or_update()
 RETURNS TRIGGER
@@ -42,3 +43,71 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE TRIGGER user_update_on_insert_or_update
 AFTER INSERT OR UPDATE ON bshop.user
 FOR EACH ROW EXECUTE FUNCTION bshop.write_user_history_on_insert_or_update();
+
+---------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION bshop.update_product_rating_and_amount_rating()
+RETURNS TRIGGER
+AS $$
+BEGIN
+    UPDATE bshop.product
+    SET rating = (rating * rating_amount + NEW.mark) / (rating_amount + 1),
+        rating_amount = rating_amount + 1
+    WHERE product_id = NEW.product_id;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER product_rating_update
+AFTER INSERT ON bshop.review
+FOR EACH ROW EXECUTE FUNCTION bshop.write_user_history_on_insert_or_update();
+
+-------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION bshop.write_product_history_on_delete()
+RETURNS TRIGGER
+AS $$
+BEGIN
+    INSERT INTO bshop.user_history VALUES (
+        OLD.product_id,
+        OLD.category,
+        OLD.name,
+        OLD.brand,
+        OLD.price,
+        OLD.available,
+        OLD.rating,
+        OLD.rating_amount,
+        NOW()
+    );
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER product_update_on_delete
+AFTER DELETE ON bshop.product
+FOR EACH ROW EXECUTE FUNCTION bshop.write_product_history_on_delete();
+
+---------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION bshop.write_product_history_on_insert_or_update()
+RETURNS TRIGGER
+AS $$
+BEGIN
+    INSERT INTO bshop.user_history VALUES (
+        NEW.product_id,
+        NEW.category,
+        NEW.name,
+        NEW.brand,
+        NEW.price,
+        NEW.available,
+        NEW.rating,
+        NEW.rating_amount,
+        NOW()
+    );
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER product_update_on_delete
+AFTER DELETE ON bshop.product
+FOR EACH ROW EXECUTE FUNCTION bshop.write_product_history_on_insert_or_update();
