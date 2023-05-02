7d3e6ce3d20533c7824c839dd636335b723bdfd6
AMKovcheg <n.dundukov@yandex.ru>
2023-05-02 17:21:11 +0300

    js added

--- ui/static/js/auth_submit.js
+++ ui/static/js/auth_submit.js
@@ -1,3 +1,28 @@
+function setCookie(name, value, options = {}) {
+
+    options = {
+      path: '/',
+      // add other defaults here if necessary
+      ...options
+    };
+  
+    if (options.expires instanceof Date) {
+      options.expires = options.expires.toUTCString();
+    }
+  
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+  
+    for (let optionKey in options) {
+      updatedCookie += "; " + optionKey;
+      let optionValue = options[optionKey];
+      if (optionValue !== true) {
+        updatedCookie += "=" + optionValue;
+      }
+    }
+  
+    document.cookie = updatedCookie;
+  }
+
 function auth_submit() {
     var inputForm = document.getElementById("auth_form");
     inputForm.addEventListener("submit", async (e)=> {
@@ -13,11 +38,12 @@ function auth_submit() {
             response => response.json()
         ).then(
             (data) => {
-                console.log(data)
-                if (data.status == 403) {
-                    document.getElementById("serverMessageBox").innerHTML="Login failed!"
-                } else {
-                    document.getElementById("serverMessageBox").innerHTML="Login succeeded!"
+                document.getElementById("serverMessageBox").innerHTML=data.description;
+                if (data.status != 403) {
+                    setCookie("username", data.userName);
+                    setCookie("token", data.token);
+                    console.log(document.cookie);
+                    window.location.replace("/");
                 }
             }
         ).catch(
